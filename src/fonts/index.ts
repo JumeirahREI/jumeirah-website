5f21217af73edc58af008f2db33824b033dd86cc
NashNoman <nashwan2k@gmail.com>
2025-08-03 19:17:51 +0300

    fix: set Manrope font subset from next/font/google

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -2,6 +2,7 @@ import { Manrope } from "next/font/google";
 import localFont from "next/font/local";
 
 export const manropeFont = Manrope({
+  subsets: ["greek"],
   variable: "--font-manrope",
 });
 
