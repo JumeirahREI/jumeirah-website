1b5ba37a7c3ce617146113a976f0712a3aab47a6
NashNoman <nashwan2k@gmail.com>
2025-10-15 19:27:48 +0300

    feat: change the logo to the official one

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,7 @@
+// import logo from "@/../public/svg/logo.svg";
+import logo from "@/../public/svg/jumeirah-logo.svg";
 import { useTranslations } from "next-intl";
 import Image from "next/image";
-// import logo from "@/../public/images/logo.png";
-import logo from "@/../public/svg/jumeirah-logo.svg";
 
 export default function Logo({ className }: { className?: string }) {
   const t = useTranslations("Common");
